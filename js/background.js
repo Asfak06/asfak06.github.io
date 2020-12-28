790885f2fc296c81a224d242b51993616d841ebf
Asfak06 <Asfakmunna@gmail.com>
2020-12-28 20:01:43 +0600

    portfolio 2.0

--- js/background.js
+++ js/background.js
@@ -9,7 +9,8 @@
   var background = document.getElementById("bgCanvas"),
       bgCtx = background.getContext("2d"),
       width = window.innerWidth,
-      height = document.body.offsetHeight;
+      // height = document.body.offsetHeight;
+      height = 450;
 
   (height < 400) ? height = 400 : height;
 
@@ -25,7 +26,7 @@
 
       this.terrain.width = width;
       this.terrain.height = height;
-      this.fillStyle = options.fillStyle || "#191D4C";
+      this.fillStyle = options.fillStyle || "#7f8c8d";
       this.mHeight = options.mHeight || height;
 
       // generate
@@ -75,89 +76,21 @@
   }
 
 
-  // Second canvas used for the stars
+// Second canvas used for the stars
   bgCtx.fillStyle = '#05004c';
   bgCtx.fillRect(0, 0, width, height);
 
-  // stars
-  function Star(options) {
-      this.size = Math.random() * 2;
-      this.speed = Math.random() * .05;
-      this.x = options.x;
-      this.y = options.y;
-  }
-
-  Star.prototype.reset = function () {
-      this.size = Math.random() * 2;
-      this.speed = Math.random() * .05;
-      this.x = width;
-      this.y = Math.random() * height;
-  }
-
-  Star.prototype.update = function () {
-      this.x -= this.speed;
-      if (this.x < 0) {
-          this.reset();
-      } else {
-          bgCtx.fillRect(this.x, this.y, this.size, this.size);
-      }
-  }
-
-  function ShootingStar() {
-      this.reset();
-  }
-
-  ShootingStar.prototype.reset = function () {
-      this.x = Math.random() * width;
-      this.y = 0;
-      this.len = (Math.random() * 80) + 10;
-      this.speed = (Math.random() * 10) + 6;
-      this.size = (Math.random() * 1) + 0.1;
-      // this is used so the shooting stars arent constant
-      this.waitTime = new Date().getTime() + (Math.random() * 3000) + 500;
-      this.active = false;
-  }
-
-  ShootingStar.prototype.update = function () {
-      if (this.active) {
-          this.x -= this.speed;
-          this.y += this.speed;
-          if (this.x < 0 || this.y >= height) {
-              this.reset();
-          } else {
-              bgCtx.lineWidth = this.size;
-              bgCtx.beginPath();
-              bgCtx.moveTo(this.x, this.y);
-              bgCtx.lineTo(this.x + this.len, this.y - this.len);
-              bgCtx.stroke();
-          }
-      } else {
-          if (this.waitTime < new Date().getTime()) {
-              this.active = true;
-          }
-      }
-  }
 
   var entities = [];
 
-  // init the stars
-  for (var i = 0; i < height; i++) {
-      entities.push(new Star({
-          x: Math.random() * width,
-          y: Math.random() * height
-      }));
-  }
 
-  // Add 2 shooting stars that just cycle.
-  entities.push(new ShootingStar());
-  entities.push(new ShootingStar());
 entities.push(new Terrain({mHeight : (height/2)-120}));
-entities.push(new Terrain({displacement : 120, scrollDelay : 50, fillStyle : "rgb(17,20,40)", mHeight : (height/2)-60}));
-  entities.push(new Terrain({displacement : 100, scrollDelay : 20, fillStyle : "rgb(10,10,5)", mHeight : height/2}));
+entities.push(new Terrain({displacement : 120, scrollDelay : 50, fillStyle : "rgb(149, 165, 166)", mHeight : (height/2)-60}));
+  entities.push(new Terrain({displacement : 100, scrollDelay : 20, fillStyle : "rgb(45, 52, 54)", mHeight : height/2}));
 
   //animate background
   function animate() {
-      bgCtx.fillStyle = '#110E19';
+      bgCtx.fillStyle = '#F5F6F7';
       bgCtx.fillRect(0, 0, width, height);
       bgCtx.fillStyle = '#ffffff';
       bgCtx.strokeStyle = '#ffffff';
